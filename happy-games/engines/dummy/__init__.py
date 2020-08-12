1834511a1989bef9b830d3142ed87ce3e6bc6875
mrader1248 <mrader1248@gmail.com>
2020-08-12 18:10:16 +0200

    first dummy game implementation

--- happy-games/engines/dummy/__init__.py
+++ happy-games/engines/dummy/__init__.py
@@ -1,6 +1,52 @@
+import asyncio
+import json
+from sanic.log import logger
+
+
 TITLE = "Dummy Game"
 
 
 class Game:
     def __init__(self):
-        pass
+        self._players = dict()
+        self._lock_add_player = asyncio.Lock()
+        self._max_players = 4
+
+    async def add_player(self, name, player):
+        async with self._lock_add_player:
+            if name in self._players:
+                logger.info(f"player '{name}' is already in game")
+                return False
+            if len(self._players) == self._max_players:
+                logger.info(f"maximum number of players reached")
+                return False
+            self._players[name] = player
+            return True
+
+    def get_player(self, name):
+        return self._players.get(name)
+
+    def broadcast(self, message):
+        for name, player in self._players.items():
+            player.notify(message)
+
+
+class WebsocketPlayer:
+    def __init__(self, user, game):
+        self._user = user
+        self._game = game
+
+    async def run(self, websocket):
+        self._websocket = websocket
+        try:
+            while True:
+                data = await websocket.recv()
+                # TODO: check if data = {"message": "..."}
+                data = json.loads(data)
+                self._game.broadcast({"message": data["message"], "user": self._user})
+        except asyncio.CancelledError as ex:
+            self._websocket = None
+            logger.info(f"WebsocketPlayer {self._user} client closed websocket connection")
+    
+    def notify(self, message):
+        asyncio.create_task(self._websocket.send(json.dumps(message)))
--- happy-games/main.py
+++ happy-games/main.py
@@ -1,9 +1,13 @@
+import asyncio
 import importlib
 import os
 import uuid
+from json import dumps as json_dumps
+from json import loads as json_loads
 from sanic import Sanic
 from sanic.log import logger
 from sanic.response import json
+from sanic.websocket import WebSocketProtocol
 
 
 app = Sanic("happy-games")
@@ -15,12 +19,15 @@ ENGINE_PACKAGES = {
     for name in os.listdir(ENGINE_DIR)
 }
 
-games_by_gameid = dict()
+for name in ENGINE_PACKAGES:
+    app.static(f"/{name}/static", f"engines/{name}/static")
+
+game_and_engine_by_gameid = dict()
 games_by_user = dict()
 
 
 @app.route("/engine", methods=["GET"])
-async def list_available_games(request):
+async def list_available_engines(request):
     return json({
         "status": "ok",
         "result": [
@@ -35,7 +42,8 @@ async def list_available_games(request):
 
 @app.route("/game", methods=["POST"])
 async def create_game(request):
-    args = request.form
+
+    args = request.json
 
     user = args.get("user")
     if user is None:
@@ -58,9 +66,16 @@ async def create_game(request):
             "result": f"unknown game '{engine_name}'"
         })
 
-    game_id = uuid.uuid4()
     game = engine.Game()
-    games_by_gameid[game_id] = game
+    player = engine.WebsocketPlayer(user, game)
+    if not await game.add_player(user, player):
+        return json({
+            "status": "error",
+            "result": f"internal error"
+        })
+
+    game_id = uuid.uuid4()
+    game_and_engine_by_gameid[game_id] = (game, engine)
     games_by_user[user] = game
 
     return json({
@@ -71,6 +86,61 @@ async def create_game(request):
     })
 
 
+@app.websocket("/game-socket")
+async def game_socket(request, websocket):
+    try:
+        game_request = json_loads(await websocket.recv())
+        
+        user = game_request.get("user")
+        if user is None:
+            await websocket.send(json_dumps({
+                "status": "error"
+            }))
+            return
+
+        game_id = game_request.get("game-id")
+        if game_id is None:
+            await websocket.send(json_dumps({
+                "status": "error"
+            }))
+            return
+        
+        game_id = uuid.UUID(game_id)
+        game_and_engine = game_and_engine_by_gameid.get(game_id)
+        if game_and_engine is None:
+            await websocket.send(json_dumps({
+                "status": "error",
+                "message": "invalid game id"
+            }))
+            return
+        game, engine = game_and_engine
+
+        player = game.get_player(user)
+        if player is None:
+            player = engine.WebsocketPlayer(user, game)
+            if not await game.add_player(user, player):
+                await websocket.send(json_dumps({
+                    "status": "error",
+                    "message": "could not join game"
+                }))
+                return
+            logger.info(f"user {user} joined game {game_id}")
+        logger.info(f"user {user} connected to game")
+        
+        asyncio.create_task(websocket.send(json_dumps({
+            "status": "ok"
+        })))
+        await player.run(websocket)
+
+    except asyncio.CancelledError as ex:
+        print("websocket connection closed by client")
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, auto_reload=True)
+    app.run(
+        host="0.0.0.0", 
+        port=8080, 
+        auto_reload=True, 
+        protocol=WebSocketProtocol
+    )
 
